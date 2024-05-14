dfda38981e58f9ebb53e19829bf77cb7e031be13
NjiahDerrick <tnjiah@gmail.com>
2024-05-14 13:01:27 +0200

    commit

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import galeryImg from "../images/galery-img.png"
+// import galeryImg from '../images/galery-img.png'
+import ImageSlider from "./ImageSlider";
+
 
 export default function Menu() {
+     
+          const slides = [
+            { url: "http://localhost:3000/image-1.jpg", title: "beach" },
+            { url: "http://localhost:3000/image-2.jpg", title: "boat" },
+            { url: "http://localhost:3000/image-3.jpg", title: "forest" },
+            { url: "http://localhost:3000/image-4.jpg", title: "city" },
+            { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+          ];
+
+          const containerStyles = {
+            width: "500px",
+            height: "280px",
+            margin: "0 auto",
+          };
   return (
-    <section id='menu' className='galery'>
+      <section id='menu' className='galery'>
            <h1 className="galery--title">
              Un événement avec<br />
              <span className="galery--title--design1">traiteur</span> c'est un<br />
@@ -13,7 +29,14 @@ export default function Menu() {
             Nous vous garantissons un événement réussi grâce a<br />
             notre service de qualité fait par des mains d'experts.
           </p>
-          <img src={galeryImg} alt="" className='galery-img' />
-    </section>
+          {/* <img src={galeryImg} alt="" className='galery-img' /> */}
+           
+            
+          <div style={containerStyles}>
+            <ImageSlider slides={slides} />
+          </div>
+          
+      </section>  
+    
   )
 }
